c3f0d27165d029749d40ba3dfdb29ec9c34f6c15
Jonas Kvinge <jonas@jkvinge.net>
2018-03-14 00:39:16 +0100

    Remove files.

--- cmake/Deb.cmake
+++
@@ -1,9 +0,0 @@
-set(DEB_ARCH amd64 CACHE STRING "Architecture of the deb file")
-set(DEB_DIST "unstable" CACHE STRING "Distribution to set in the .deb changelog")
-
-add_custom_target(deb
-  COMMAND dpkg-buildpackage -b -d -uc -us
-  COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_SOURCE_DIR}/../strawberry_${STRAWBERRY_VERSION_DEB}~${DEB_DIST}_${DEB_ARCH}.deb
-                                   ${CMAKE_BINARY_DIR}/strawberry_${STRAWBERRY_VERSION_DEB}~${DEB_DIST}_${DEB_ARCH}.deb
-  WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-)
