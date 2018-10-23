51021131d779f26a54c5583ee1bab4c08376c6ef
Jonas Kvinge <jonas@jkvinge.net>
2018-10-23 20:17:29 +0200

    Allow compile on Qt 5.5.1 or above

--- src/widgets/stretchheaderview.cpp
+++ src/widgets/stretchheaderview.cpp
@@ -246,7 +246,11 @@ void StretchHeaderView::SetColumnWidth(int logical, ColumnWidthType width) {
 bool StretchHeaderView::RestoreState(const QByteArray& data) {
 
   QDataStream s(data);
+#if QT_VERSION < QT_VERSION_CHECK(5, 6, 0)
+  s.setVersion(QDataStream::Qt_5_5);
+#else
   s.setVersion(QDataStream::Qt_5_6);
+#endif
 
   int magic_number = 0;
   s >> magic_number;
@@ -310,7 +314,11 @@ QByteArray StretchHeaderView::SaveState() const {
     visual_indices << logicalIndex(i);
   }
 
+#if QT_VERSION < QT_VERSION_CHECK(5, 6, 0)
+  s.setVersion(QDataStream::Qt_5_5);
+#else
   s.setVersion(QDataStream::Qt_5_6);
+#endif
   s << kMagicNumber;
 
   s << stretch_enabled_;
