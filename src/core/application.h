2e1b6015087a2aa0f1ac96009a013f784f6b4eba
Jonas Kvinge <jonas@jkvinge.net>
2018-12-29 03:25:21 +0100

    Fix copyright header

--- src/core/application.h
+++ src/core/application.h
@@ -2,7 +2,7 @@
  * Strawberry Music Player
  * This file was part of Clementine.
  * Copyright 2012, David Sansome <me@davidsansome.com>
-   Copyright 2012, 2014, John Maguire <john.maguire@gmail.com>
+ * Copyright 2012, 2014, John Maguire <john.maguire@gmail.com>
  * Copyright 2018, Jonas Kvinge <jonas@jkvinge.net>
  *
  * Strawberry is free software: you can redistribute it and/or modify
