3deca001a7fe0165bf99063ef6c1ca7a97e7daca
Jonas Kvinge <jonas@jkvinge.net>
2019-07-19 23:18:07 +0200

    Fix macdeployqt command

--- cmake/Dmg.cmake
+++ cmake/Dmg.cmake
@@ -1,5 +1,5 @@
 add_custom_target(dmg
-  COMMAND sudo macdeployqt strawberry.app
+  COMMAND sudo /usr/local/opt/qt5/bin/macdeployqt strawberry.app
   COMMAND sudo ${CMAKE_SOURCE_DIR}/dist/macos/create-dmg.sh strawberry.app
   WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
 )
