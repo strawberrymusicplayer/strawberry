a3ae9acebb6fe2890c4afb85bd51cb8896f63e54
Jonas Kvinge <jonas@jkvinge.net>
2019-09-29 13:50:24 +0200

    Listenbrainz: don't send "various artists" as artist

--- src/scrobbler/listenbrainzscrobbler.cpp
+++ src/scrobbler/listenbrainzscrobbler.cpp
@@ -464,8 +464,10 @@ void ListenBrainzScrobbler::Submit() {
     QJsonObject object_listen;
     object_listen.insert("listened_at", QJsonValue::fromVariant(item->timestamp_));
     QJsonObject object_track_metadata;
-    if (item->albumartist_.isEmpty()) object_track_metadata.insert("artist_name", QJsonValue::fromVariant(item->artist_));
-    else object_track_metadata.insert("artist_name", QJsonValue::fromVariant(item->albumartist_));
+    if (item->albumartist_.isEmpty() || item->albumartist_.toLower() == "various artists")
+      object_track_metadata.insert("artist_name", QJsonValue::fromVariant(item->artist_));
+    else
+      object_track_metadata.insert("artist_name", QJsonValue::fromVariant(item->albumartist_));
     object_track_metadata.insert("release_name", QJsonValue::fromVariant(item->album_));
     object_track_metadata.insert("track_name", QJsonValue::fromVariant(item->song_));
     object_listen.insert("track_metadata", object_track_metadata);
