4e593cebab944d60124b1e9d2dc0b297bcc2829a
Jonas Kvinge <jonas@jkvinge.net>
2019-11-06 21:53:09 +0100

    Add const

--- src/engine/gstenginepipeline.cpp
+++ src/engine/gstenginepipeline.cpp
@@ -141,8 +141,10 @@ GstEnginePipeline::~GstEnginePipeline() {
       g_signal_handler_disconnect(G_OBJECT(pipeline_), notify_source_cb_id_);
 
     gst_bus_set_sync_handler(gst_pipeline_get_bus(GST_PIPELINE(pipeline_)), nullptr, nullptr, nullptr);
+
     if (bus_cb_id_ != -1)
       g_source_remove(bus_cb_id_);
+
     gst_element_set_state(pipeline_, GST_STATE_NULL);
 
     gst_object_unref(GST_OBJECT(pipeline_));
@@ -956,8 +958,7 @@ GstState GstEnginePipeline::state() const {
 
 }
 
-QFuture<GstStateChangeReturn> GstEnginePipeline::SetState(GstState state) {
-
+QFuture<GstStateChangeReturn> GstEnginePipeline::SetState(const GstState state) {
   return ConcurrentRun::Run<GstStateChangeReturn, GstElement*, GstState>(&set_state_threadpool_, &gst_element_set_state, pipeline_, state);
 
 }
