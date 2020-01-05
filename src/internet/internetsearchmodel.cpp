b5a7945e49a59ea35d734a50b0d74b64395dd8dc
Jonas Kvinge <jonas@jkvinge.net>
2020-01-05 23:25:23 +0100

    Use QModelIndex::model()

--- src/internet/internetsearchmodel.cpp
+++ src/internet/internetsearchmodel.cpp
@@ -312,7 +312,7 @@ void InternetSearchModel::GetChildResults(const QStandardItem *item, InternetSea
 
     // Yes - visit all the children, but do so through the proxy so we get them in the right order.
     for (int i = 0; i < item->rowCount(); ++i) {
-      const QModelIndex proxy_index = proxy_->index(i, 0, parent_proxy_index);
+      const QModelIndex proxy_index = parent_proxy_index.model()->index(i, 0, parent_proxy_index);
       const QModelIndex index = proxy_->mapToSource(proxy_index);
       GetChildResults(itemFromIndex(index), results, visited);
     }
