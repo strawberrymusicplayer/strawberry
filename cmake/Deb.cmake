5fd61725bdee2dc5d6830a6aaa2535a244516a0d
Jonas Kvinge <jonas@jkvinge.net>
2020-01-21 23:15:15 +0100

    Fixes for debian packaging

--- cmake/Deb.cmake
+++ cmake/Deb.cmake
@@ -1,5 +1,15 @@
-add_custom_target(deb
-  COMMAND cp -r -p ${CMAKE_SOURCE_DIR}/debian ${CMAKE_BINARY_DIR}/
-  COMMAND dpkg-buildpackage -b -d -uc -us
-  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-)
+find_program(LSB_RELEASE_EXEC lsb_release)
+find_program(DPKG_BUILDPACKAGE dpkg-buildpackage)
+
+if (LSB_RELEASE_EXEC AND DPKG_BUILDPACKAGE)
+  execute_process(COMMAND /bin/sh "-c" "${LSB_RELEASE_EXEC} -cs"
+    OUTPUT_VARIABLE DEB_CODENAME
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
+  if (DEB_CODENAME)
+    add_custom_target(deb
+      WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+      COMMAND ${DPKG_BUILDPACKAGE} -b -d -uc -us
+    )
+  endif()
+endif()
