b50081361713e36f481a03e98c54b300a2a34f28
Jonas Kvinge <jonas@jkvinge.net>
2020-04-08 00:56:19 +0200

    Disable open Audio CD on Windows

--- src/core/mainwindow.cpp
+++ src/core/mainwindow.cpp
@@ -985,7 +985,11 @@ void MainWindow::ExitFinished() {
 void MainWindow::EngineChanged(Engine::EngineType enginetype) {
 
   ui_->action_equalizer->setEnabled(enginetype == Engine::EngineType::GStreamer || enginetype == Engine::EngineType::Xine);
+#ifdef Q_OS_WIN
+  ui_->action_open_cd->setEnabled(false);
+#else
   ui_->action_open_cd->setEnabled(enginetype == Engine::EngineType::GStreamer);
+#endif
 
 }
 
