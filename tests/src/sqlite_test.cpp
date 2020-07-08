2fc6f835a8c7ab93dfb81079fa035b75daa998df
Jonas Kvinge <jonas@jkvinge.net>
2020-07-08 17:46:04 +0200

    Update sqlite test to use FTS5 instead of FTS3

--- tests/src/sqlite_test.cpp
+++ tests/src/sqlite_test.cpp
@@ -1,14 +1,14 @@
 #include <gtest/gtest.h>
 #include <sqlite3.h>
 
-TEST(SqliteTest, FTS3SupportEnabled) {
+TEST(SqliteTest, FTS5SupportEnabled) {
 
   sqlite3* db = nullptr;
   int rc = sqlite3_open(":memory:", &db);
   ASSERT_EQ(0, rc);
 
   char* errmsg = nullptr;
-  rc = sqlite3_exec(db, "CREATE VIRTUAL TABLE foo USING fts3(content, TEXT)", nullptr, nullptr, &errmsg);
+  rc = sqlite3_exec(db, "CREATE VIRTUAL TABLE foo USING fts5(content, TEXT, tokenize = 'unicode61 remove_diacritics 0')", nullptr, nullptr, &errmsg);
   ASSERT_EQ(0, rc) << errmsg;
 
   sqlite3_close(db);
