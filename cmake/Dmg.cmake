b4125fa56c4f1e884c9937d68248a5b23072fd85
Jonas Kvinge <jonas@jkvinge.net>
2020-08-01 03:31:01 +0200

    Remove create-dmg.sh script and use create-dmg directly from CMake

--- cmake/Dmg.cmake
+++ cmake/Dmg.cmake
@@ -1,6 +1,6 @@
 add_custom_target(dmg
   COMMAND /usr/local/opt/qt5/bin/macdeployqt strawberry.app
   COMMAND ${CMAKE_SOURCE_DIR}/dist/macos/macdeploy.py strawberry.app
-  COMMAND ${CMAKE_SOURCE_DIR}/dist/macos/create-dmg.sh strawberry.app
+  COMMAND create-dmg --volname strawberry --background "${CMAKE_SOURCE_DIR}/dist/macos/dmg_background.png" --app-drop-link 450 218 --icon strawberry.app 150 218 --window-size 600 450 strawberry-${STRAWBERRY_VERSION_PACKAGE}.dmg strawberry.app
   WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
 )
