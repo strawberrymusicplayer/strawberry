be966488e8fcfcffb2455e8484b0208a8ffd0f54
Jonas Kvinge <jonas@jkvinge.net>
2020-08-10 23:05:07 +0200

    Fix OSD D-Bus assertion with Qt 6

--- src/osd/osddbus.cpp
+++ src/osd/osddbus.cpp
@@ -83,7 +83,7 @@ QDBusArgument &operator<<(QDBusArgument &arg, const QImage &image) {
   arg.beginStructure();
   arg << i.width();
   arg << i.height();
-  arg << i.bytesPerLine();
+  arg << static_cast<qint32>(i.bytesPerLine());
   arg << i.hasAlphaChannel();
   int channels = i.isGrayscale() ? 1 : (i.hasAlphaChannel() ? 4 : 3);
   arg << i.depth() / channels;
