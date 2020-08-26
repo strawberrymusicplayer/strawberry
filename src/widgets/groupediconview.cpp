e52cda193e1fd64ee60e843564832e3fd54463d3
Jonas Kvinge <jonas@jkvinge.net>
2020-08-26 23:35:33 +0200

    Replace QAbstractItemView::viewOptions with initViewItemOption

--- src/widgets/groupediconview.cpp
+++ src/widgets/groupediconview.cpp
@@ -230,7 +230,12 @@ QModelIndex GroupedIconView::indexAt(const QPoint &p) const {
 void GroupedIconView::paintEvent(QPaintEvent *e) {
   // This code was adapted from QListView::paintEvent(), changed to use the visualRect() of items, and to draw headers.
 
-  QStyleOptionViewItem option(viewOptions());
+  QStyleOptionViewItem option;
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+  initViewItemOption(&option);
+#else
+  option = viewOptions();
+#endif
   if (isWrapping())
     option.features = QStyleOptionViewItem::WrapText;
   option.locale = locale();
