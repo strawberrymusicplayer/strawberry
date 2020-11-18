742d455b0e09a35a3f9b21d9ce14e3fa1bc1362c
Jonas Kvinge <jonas@jkvinge.net>
2020-11-18 21:51:51 +0100

    Build on Big Sur

--- dist/macos/macversion.sh
+++ dist/macos/macversion.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 
-macos_version=$(sw_vers -productVersion| awk -F '[.]' '{print $2}')
-macos_codenames=(
-["13"]="highsierra"
-["14"]="mojave"
-["15"]="catalina"
-)
+macos_version=$(sw_vers -productVersion)
+macos_version_major=$(echo $macos_version | awk -F '[.]' '{print $1}')
+macos_version_minor=$(echo $macos_version | awk -F '[.]' '{print $2}')
 
-if [[ -n "${macos_codenames[$macos_version]}" ]]; then 
-  echo "${macos_codenames[$macos_version]}"
+if [ "${macos_version_major}" = "10" ]; then
+  macos_codenames=(
+    ["13"]="highsierra"
+    ["14"]="mojave"
+    ["15"]="catalina"
+  )
+  if [[ -n "${macos_codenames[$macos_version_minor]}" ]]; then
+    echo "${macos_codenames[$macos_version_minor]}"
+  else
+    echo "unknown"
+  fi
+elif [ "${macos_version_major}" = "11" ]; then
+  echo "bigsur"
 else
   echo "unknown"
 fi
