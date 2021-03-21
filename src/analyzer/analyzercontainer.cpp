e90b5e63e53ee6dd2baefd4b892b1b5c61883153
Jonas Kvinge <jonas@jkvinge.net>
2021-03-21 04:49:22 +0100

    Move variable

--- src/analyzer/analyzercontainer.cpp
+++ src/analyzer/analyzercontainer.cpp
@@ -191,10 +191,10 @@ void AnalyzerContainer::Load() {
   }
 
   // Framerate
+  QList<QAction*> actions = group_framerate_->actions();
   for (int i = 0; i < framerate_list_.count(); ++i) {
     if (current_framerate_ == framerate_list_[i]) {
       ChangeFramerate(current_framerate_);
-      QList<QAction*> actions = group_framerate_->actions();
       actions[i]->setChecked(true);
       break;
     }
