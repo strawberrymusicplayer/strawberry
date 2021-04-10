88da0db0f70c55ba94b8edeeaaca8de24a62ae98
Jonas Kvinge <jonas@jkvinge.net>
2021-04-10 07:32:38 +0200

    Update copyrights

--- ext/libstrawberry-common/core/messagehandler.cpp
+++ ext/libstrawberry-common/core/messagehandler.cpp
@@ -1,5 +1,6 @@
 /* This file is part of Strawberry.
    Copyright 2011, David Sansome <me@davidsansome.com>
+   Copyright 2018-2021, Jonas Kvinge <jonas@jkvinge.net>
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
