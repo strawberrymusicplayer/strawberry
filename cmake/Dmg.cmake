63d75a8e1722cfbe86d111e6277508873e98cb0c
Jonas Kvinge <jonas@jkvinge.net>
2021-04-23 19:05:02 +0200

    Build common DMG for all macOS versions

--- cmake/Dmg.cmake
+++ cmake/Dmg.cmake
@@ -1,11 +1,10 @@
 #find_program(MACDEPLOYQT_EXECUTABLE NAMES macdeployqt PATHS /usr/local/opt/qt6/bin /usr/local/opt/qt5/bin /usr/local/bin REQUIRED)
-#if(MACDEPLOYQT_EXECUTABLE)
-#  message(STATUS "Found macdeployqt: ${MACDEPLOYQT_EXECUTABLE}")
-#else()
-#  message(WARNING "Missing macdeployqt executable.")
-#endif()
-
 set(MACDEPLOYQT_EXECUTABLE "${CMAKE_BINARY_DIR}/3rdparty/macdeployqt/macdeployqt")
+if(MACDEPLOYQT_EXECUTABLE)
+  message(STATUS "Found macdeployqt: ${MACDEPLOYQT_EXECUTABLE}")
+else()
+  message(WARNING "Missing macdeployqt executable.")
+endif()
 
 find_program(CREATEDMG_EXECUTABLE NAMES create-dmg REQUIRED)
 if(CREATEDMG_EXECUTABLE)
@@ -14,24 +13,12 @@ else()
   message(WARNING "Missing create-dmg executable.")
 endif()
 
-execute_process(COMMAND ${CMAKE_SOURCE_DIR}/dist/macos/macversion.sh OUTPUT_VARIABLE MACOS_VERSION_PACKAGE OUTPUT_STRIP_TRAILING_WHITESPACE)
-if(NOT MACOS_VERSION_PACKAGE)
-  message(WARNING "Could not set macOS version.")
-endif()
-
-if(MACDEPLOYQT_EXECUTABLE AND CREATEDMG_EXECUTABLE AND MACOS_VERSION_PACKAGE)
+if(MACDEPLOYQT_EXECUTABLE AND CREATEDMG_EXECUTABLE)
   add_custom_target(dmg
     COMMAND mkdir -p ${CMAKE_BINARY_DIR}/strawberry.app/Contents/Frameworks/
     COMMAND cp -r /usr/local/opt/sparkle/Sparkle.framework ${CMAKE_BINARY_DIR}/strawberry.app/Contents/Frameworks/
     COMMAND ${MACDEPLOYQT_EXECUTABLE} strawberry.app -verbose=3 -executable=${CMAKE_BINARY_DIR}/strawberry.app/Contents/PlugIns/strawberry-tagreader
-    COMMAND ${CREATEDMG_EXECUTABLE} --volname strawberry --background "${CMAKE_SOURCE_DIR}/dist/macos/dmg_background.png" --app-drop-link 450 218 --icon strawberry.app 150 218 --window-size 600 450 strawberry-${STRAWBERRY_VERSION_PACKAGE}-${MACOS_VERSION_PACKAGE}-${CMAKE_HOST_SYSTEM_PROCESSOR}.dmg strawberry.app
-    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-  )
-  add_custom_target(dmg2
-    COMMAND mkdir -p ${CMAKE_BINARY_DIR}/strawberry.app/Contents/Frameworks/
-    COMMAND cp -r /usr/local/opt/sparkle/Sparkle.framework ${CMAKE_BINARY_DIR}/strawberry.app/Contents/Frameworks/
-    COMMAND ${MACDEPLOYQT_EXECUTABLE} strawberry.app -verbose=3 -executable=${CMAKE_BINARY_DIR}/strawberry.app/Contents/PlugIns/strawberry-tagreader
-    COMMAND ${CREATEDMG_EXECUTABLE} --skip-jenkins --volname strawberry --background "${CMAKE_SOURCE_DIR}/dist/macos/dmg_background.png" --app-drop-link 450 218 --icon strawberry.app 150 218 --window-size 600 450 strawberry-${STRAWBERRY_VERSION_PACKAGE}-${MACOS_VERSION_PACKAGE}-${CMAKE_HOST_SYSTEM_PROCESSOR}.dmg strawberry.app
+    COMMAND ${CREATEDMG_EXECUTABLE} --volname strawberry --background "${CMAKE_SOURCE_DIR}/dist/macos/dmg_background.png" --app-drop-link 450 218 --icon strawberry.app 150 218 --window-size 600 450 strawberry-${STRAWBERRY_VERSION_PACKAGE}-${CMAKE_HOST_SYSTEM_PROCESSOR}.dmg strawberry.app
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
   )
 endif()
