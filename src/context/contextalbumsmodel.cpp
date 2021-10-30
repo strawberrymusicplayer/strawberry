a704412deeaa11bdf279758784c173c5dcf50365
Jonas Kvinge <jonas@jkvinge.net>
2021-10-30 02:05:55 +0200

    ContextAlbumsModel: Use `const_iterator`

--- src/context/contextalbumsmodel.cpp
+++ src/context/contextalbumsmodel.cpp
@@ -247,7 +247,7 @@ QVariant ContextAlbumsModel::data(const CollectionItem *item, int role) const {
 
 void ContextAlbumsModel::Reset() {
 
-  for (QMap<QString, CollectionItem*>::iterator it = container_nodes_.begin(); it != container_nodes_.end(); ++it) {
+  for (QMap<QString, CollectionItem*>::const_iterator it = container_nodes_.constBegin(); it != container_nodes_.constEnd(); ++it) {
     const QString cache_key = AlbumIconPixmapCacheKey(ItemToIndex(it.value()));
     QPixmapCache::remove(cache_key);
   }
