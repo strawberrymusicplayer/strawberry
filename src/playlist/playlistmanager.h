962b52bd5b8a45f854bca064c6934d57593acfb4
Maxime Haselbauer <renn0xtek9@laposte.net>
2022-03-13 11:28:17 +0100

    Add save all playlists action

--- src/playlist/playlistmanager.h
+++ src/playlist/playlistmanager.h
@@ -222,6 +222,8 @@ class PlaylistManager : public PlaylistManagerInterface {
   // Rate current song using 0 - 5 scale.
   void RateCurrentSong2(const int rating) override;
 
+  void SaveAllPlaylists();
+
  private slots:
   void SetActivePlaying() override;
   void SetActivePaused() override;
