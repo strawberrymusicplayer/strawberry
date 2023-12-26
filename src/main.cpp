a35fa5b15876323d88a5cce8198b0207ef1dfdf7
Jonas Kvinge <jonas@jkvinge.net>
2023-12-26 20:47:11 +0100

    Require KDSingleApplication 1.1.0

--- src/main.cpp
+++ src/main.cpp
@@ -156,11 +156,7 @@ int main(int argc, char *argv[]) {
     // Only start a core application now, so we can check if there's another instance without requiring an X server.
     // This MUST be done before parsing the commandline options so QTextCodec gets the right system locale for filenames.
     QCoreApplication core_app(argc, argv);
-#ifdef HAVE_KDSINGLEAPPLICATION_OPTIONS
     KDSingleApplication single_app(QCoreApplication::applicationName(), KDSingleApplication::Option::IncludeUsernameInSocketName);
-#else
-    KDSingleApplication single_app(QCoreApplication::applicationName());
-#endif
     // Parse commandline options - need to do this before starting the full QApplication, so it works without an X server
     if (!options.Parse()) return 1;
     logging::SetLevels(options.log_levels());
@@ -197,11 +193,7 @@ int main(int argc, char *argv[]) {
   QGuiApplication::setQuitOnLastWindowClosed(false);
 
   QApplication a(argc, argv);
-#ifdef HAVE_KDSINGLEAPPLICATION_OPTIONS
   KDSingleApplication single_app(QCoreApplication::applicationName(), KDSingleApplication::Option::IncludeUsernameInSocketName);
-#else
-  KDSingleApplication single_app(QCoreApplication::applicationName());
-#endif
   if (!single_app.isPrimaryInstance()) {
     if (options.is_empty()) {
       qLog(Info) << "Strawberry is already running - activating existing window (2)";
