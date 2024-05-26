6e90e72b4a366b3d4df342c6af8d1a49841c45d8
Jonas Kvinge <jonas@jkvinge.net>
2024-05-26 02:49:26 +0200

    CollectionModel: Add content to fake header for pixmap cache
    QNetworkCacheMetaData requires this now.

--- src/collection/collectionmodel.cpp
+++ src/collection/collectionmodel.cpp
@@ -719,7 +719,7 @@ void CollectionModel::AlbumCoverLoaded(const quint64 id, const AlbumCoverLoaderR
       disk_cache_metadata.setSaveToDisk(true);
       disk_cache_metadata.setUrl(disk_cache_key);
       // Qt 6 now ignores any entry without headers, so add a fake header.
-      disk_cache_metadata.setRawHeaders(QNetworkCacheMetaData::RawHeaderList() << qMakePair(QByteArray(), QByteArray()));
+      disk_cache_metadata.setRawHeaders(QNetworkCacheMetaData::RawHeaderList() << qMakePair(QByteArray("collection-thumbnail"), cache_key.toUtf8()));
       QIODevice *device_iconcache = sIconCache->prepare(disk_cache_metadata);
       if (device_iconcache) {
         result.image_scaled.save(device_iconcache, "XPM");
