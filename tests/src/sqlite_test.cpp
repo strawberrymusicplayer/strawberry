79c28e7e1d867df02a30e90df666e5530796effb
Jonas Kvinge <jonas@jkvinge.net>
2024-07-09 03:24:13 +0200

    sqlite_test: Remove fts5

--- tests/src/sqlite_test.cpp
+++ tests/src/sqlite_test.cpp
@@ -3,14 +3,14 @@
 
 // clazy:excludeall=returning-void-expression
 
-TEST(SqliteTest, FTS5SupportEnabled) {
+TEST(SqliteTest, CreateTableTest) {
 
-  sqlite3* db = nullptr;
+  sqlite3 *db = nullptr;
   int rc = sqlite3_open(":memory:", &db);
   ASSERT_EQ(0, rc);
 
-  char* errmsg = nullptr;
-  rc = sqlite3_exec(db, "CREATE VIRTUAL TABLE foo USING fts5(content, TEXT, tokenize = 'unicode61 remove_diacritics 0')", nullptr, nullptr, &errmsg);
+  char *errmsg = nullptr;
+  rc = sqlite3_exec(db, "CREATE TABLE foo (content TEXT)", nullptr, nullptr, &errmsg);
   ASSERT_EQ(0, rc) << errmsg;
 
   sqlite3_close(db);
