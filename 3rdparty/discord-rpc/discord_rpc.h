a7dbeb5d76c4f3883a6012a1c3754727a7a53413
Jonas Kvinge <jonas@jkvinge.net>
2025-04-11 23:37:17 +0200

    discord-rpc: Add copyright

--- 3rdparty/discord-rpc/discord_rpc.h
+++ 3rdparty/discord-rpc/discord_rpc.h
@@ -1,5 +1,30 @@
-#pragma once
-#include <stdint.h>
+/*
+ * Copyright 2017 Discord, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#ifndef DISCORD_RPC_H
+#define DISCORD_RPC_H
+
+#include <cstdint>
 
 namespace discord_rpc {
 
@@ -65,6 +90,8 @@ void Discord_UpdateHandlers(DiscordEventHandlers *handlers);
 
 #ifdef __cplusplus
 } /* extern "C" */
+#endif
+
 }  // namespace discord_rpc
 
-#endif
+#endif  // DISCORD_RPC_H
