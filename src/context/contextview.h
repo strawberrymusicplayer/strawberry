7f4f715003c58eefef3227e4203279b38a41e280
Jonas Kvinge <jonas@jkvinge.net>
2025-09-07 13:53:59 +0200

    ContextView: Remove EBU R 128
    It breaks wordwrap

--- src/context/contextview.h
+++ src/context/contextview.h
@@ -1,6 +1,6 @@
 /*
  * Strawberry Music Player
- * Copyright 2013-2022, Jonas Kvinge <jonas@jkvinge.net>
+ * Copyright 2013-2025, Jonas Kvinge <jonas@jkvinge.net>
  *
  * Strawberry is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -135,17 +135,11 @@ class ContextView : public QWidget {
   QLabel *label_bitdepth_title_;
   QLabel *label_bitrate_title_;
 
-  QLabel *label_ebur128_integrated_loudness_title_;
-  QLabel *label_ebur128_loudness_range_title_;
-
   QLabel *label_filetype_;
   QLabel *label_length_;
   QLabel *label_samplerate_;
   QLabel *label_bitdepth_;
   QLabel *label_bitrate_;
-
-  QLabel *label_ebur128_integrated_loudness_;
-  QLabel *label_ebur128_loudness_range_;
 
   Song song_playing_;
   Song song_prev_;
