e01dd298b97d4a267d97ce78ce521637fe593686
coolcoyote <coolcoyote358@gmail.com>
2026-03-15 20:43:07 +0100

    Add a sub-menu to the shuffle playlist menu to decide how to shuffle.

--- src/playlist/playlist.h
+++ src/playlist/playlist.h
@@ -290,6 +290,8 @@ class Playlist : public QAbstractListModel {
 
   void ItemReload(const QPersistentModelIndex &idx, const bool metadata_edit);
 
+  void Shuffle(const PlaylistSequence::ShuffleMode shuffle_mode = PlaylistSequence::ShuffleMode::All);
+
  public Q_SLOTS:
   void set_current_row(const int i, const Playlist::AutoScroll autoscroll = Playlist::AutoScroll::Maybe, const bool is_stopping = false, const bool force_inform = false);
   void Paused();
@@ -303,7 +305,6 @@ class Playlist : public QAbstractListModel {
   void Clear();
   void RemoveDuplicateSongs();
   void RemoveUnavailableSongs();
-  void Shuffle();
 
   void ShuffleModeChanged(const PlaylistSequence::ShuffleMode shuffle_mode);
 
@@ -355,6 +356,8 @@ class Playlist : public QAbstractListModel {
   void MoveItemWithoutUndo(const int source, const int dest);
   void MoveItemsWithoutUndo(int start, const QList<int> &dest_rows);
   void ReOrderWithoutUndo(const PlaylistItemPtrList &new_items);
+
+  int ReshuffleIndices(QList<int>& virtual_items, const PlaylistSequence::ShuffleMode shuffle_mode, const int base_reference, const bool album_keep_track_order);
 
   void RemoveItemsNotInQueue();
 
--- src/playlist/playlistmanager.cpp
+++ src/playlist/playlistmanager.cpp
@@ -409,6 +409,10 @@ void PlaylistManager::ShuffleCurrent() {
   current()->Shuffle();
 }
 
+void PlaylistManager::ShuffleCurrent(const PlaylistSequence::ShuffleMode shuffle_mode) {
+  current()->Shuffle(shuffle_mode);
+}
+
 void PlaylistManager::RemoveDuplicatesCurrent() {
   current()->RemoveDuplicateSongs();
 }
