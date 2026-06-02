033f5c7fceb7e39ba69cfb28b5bf2965e66d0118
Jonas Kvinge <jonas@jkvinge.net>
2026-06-03 01:40:33 +0200

    Use zero progress if total is less or equal to zero

--- src/tidal/tidalrequest.cpp
+++ src/tidal/tidalrequest.cpp
@@ -1316,6 +1316,7 @@ void TidalRequest::FinishCheck() {
 
 int TidalRequest::GetProgress(const int count, const int total) {
 
+  if (total <= 0) return 0;
   return static_cast<int>((static_cast<float>(count) / static_cast<float>(total)) * 100.0F);
 
 }
