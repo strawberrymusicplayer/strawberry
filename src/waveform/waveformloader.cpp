aa659e6e4f501b7d2aa576888d540614369b9da9
Octavio Calleya Garcia <octavio@transgressoft.net>
2026-06-10 10:52:52 +0300

    WaveformLoader: Add async cache orchestrator for track waveforms
    Owns a worker QThread at idle I/O priority, caps in-flight requests at
    idealThreadCount/2, dedupes by URL, and persists generated blobs to a
    QNetworkDiskCache at CacheLocation/waveform keyed by the percent-encoded
    source path. Replaying a track returns the cached blob synchronously.
    Cloned from MoodbarLoader; the .mood sidecar and save setting are dropped.

---
+++ src/waveform/waveformloader.cpp
@@ -0,0 +1,167 @@
+/*
+ * Strawberry Music Player
+ * Copyright 2026, Strawberry contributors
+ *
+ * Strawberry is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Strawberry is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Strawberry.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "waveformloader.h"
+
+#include <memory>
+
+#include <QtGlobal>
+#include <QObject>
+#include <QThread>
+#include <QCoreApplication>
+#include <QIODevice>
+#include <QAbstractNetworkCache>
+#include <QNetworkDiskCache>
+#include <QByteArray>
+#include <QString>
+#include <QUrl>
+
+#include "includes/scoped_ptr.h"
+#include "includes/shared_ptr.h"
+#include "core/logging.h"
+#include "core/standardpaths.h"
+
+#include "waveformpipeline.h"
+
+using namespace Qt::Literals::StringLiterals;
+using std::make_shared;
+
+WaveformLoader::WaveformLoader(QObject *parent)
+    : QObject(parent),
+      cache_(new QNetworkDiskCache(this)),
+      thread_(new QThread(this)),
+      kMaxActiveRequests(qMax(1, QThread::idealThreadCount() / 2)) {
+
+  setObjectName(QLatin1String(QObject::metaObject()->className()));
+  thread_->setObjectName(objectName());
+
+  cache_->setCacheDirectory(StandardPaths::WritableLocation(StandardPaths::StandardLocation::CacheLocation) + u"/waveform"_s);
+  cache_->setMaximumCacheSize(60LL * 1024LL * 1024LL);  // 60MB
+
+}
+
+WaveformLoader::~WaveformLoader() {
+  thread_->quit();
+  thread_->wait(1000);
+}
+
+QUrl WaveformLoader::CacheUrlEntry(const QString &filename) {
+
+  return QUrl(QString::fromLatin1(QUrl::toPercentEncoding(filename)));
+
+}
+
+WaveformLoader::LoadResult WaveformLoader::Load(const QUrl &url, const bool has_cue) {
+
+  if (!url.isLocalFile() || has_cue) {
+    return LoadStatus::CannotLoad;
+  }
+
+  // Are we in the middle of loading this waveform already?
+  if (requests_.contains(url)) {
+    return LoadResult(LoadStatus::WillLoadAsync, requests_.value(url));
+  }
+
+  const QString filename(url.toLocalFile());
+
+  // Maybe it exists in the cache?
+
+  QNetworkCacheMetaData disk_cache_metadata = cache_->metaData(CacheUrlEntry(filename));
+  if (disk_cache_metadata.isValid()) {
+    ScopedPtr<QIODevice> device_cache_file(cache_->data(disk_cache_metadata.url()));
+    if (device_cache_file) {
+      qLog(Debug) << "Loading cached waveform data for" << filename;
+      const QByteArray data = device_cache_file->readAll();
+      if (!data.isEmpty()) {
+        return LoadResult(LoadStatus::Loaded, data);
+      }
+    }
+  }
+
+  if (!thread_->isRunning()) thread_->start(QThread::IdlePriority);
+
+  // There was no existing data, analyze the audio file and create one.
+  WaveformPipelinePtr pipeline = WaveformPipelinePtr(new WaveformPipeline(url));
+  pipeline->moveToThread(thread_);
+  SharedPtr<QMetaObject::Connection> connection = make_shared<QMetaObject::Connection>();
+  *connection = QObject::connect(&*pipeline, &WaveformPipeline::Finished, this, [this, connection, pipeline, url]() {
+    RequestFinished(pipeline, url);
+    QObject::disconnect(*connection);
+  });
+
+  requests_[url] = pipeline;
+  queued_requests_ << url;
+
+  MaybeTakeNextRequest();
+
+  return LoadResult(LoadStatus::WillLoadAsync, pipeline);
+
+}
+
+void WaveformLoader::MaybeTakeNextRequest() {
+
+  Q_ASSERT(QThread::currentThread() == qApp->thread());
+
+  if (active_requests_.count() >= kMaxActiveRequests || queued_requests_.isEmpty()) {
+    return;
+  }
+
+  const QUrl url = queued_requests_.takeFirst();
+  active_requests_ << url;
+
+  qLog(Debug) << "Creating waveform data for" << url.toLocalFile();
+
+  WaveformPipelinePtr pipeline = requests_.value(url);
+  QMetaObject::invokeMethod(&*pipeline, &WaveformPipeline::Start, Qt::QueuedConnection);
+
+}
+
+void WaveformLoader::RequestFinished(WaveformPipelinePtr pipeline, const QUrl &url) {
+
+  Q_ASSERT(QThread::currentThread() == qApp->thread());
+
+  if (pipeline->success()) {
+
+    const QString filename = url.toLocalFile();
+
+    qLog(Debug) << "Waveform data generated successfully for" << filename;
+
+    // Save the data in the cache
+    QNetworkCacheMetaData disk_cache_metadata;
+    disk_cache_metadata.setSaveToDisk(true);
+    disk_cache_metadata.setUrl(CacheUrlEntry(filename));
+    // Qt 6 now ignores any entry without headers, so add a fake header.
+    disk_cache_metadata.setRawHeaders(QNetworkCacheMetaData::RawHeaderList() << qMakePair(QByteArray("waveform"), QByteArray("waveform")));
+
+    QIODevice *device_cache_file = cache_->prepare(disk_cache_metadata);
+    if (device_cache_file) {
+      const qint64 data_written = device_cache_file->write(pipeline->data());
+      if (data_written > 0) {
+        cache_->insert(device_cache_file);
+      }
+    }
+  }
+
+  // Remove the request from the active list and delete it
+  requests_.remove(url);
+  active_requests_.remove(url);
+
+  MaybeTakeNextRequest();
+
+}
---
+++ src/waveform/waveformloader.h
@@ -0,0 +1,89 @@
+/*
+ * Strawberry Music Player
+ * Copyright 2026, Strawberry contributors
+ *
+ * Strawberry is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Strawberry is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Strawberry.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef WAVEFORMLOADER_H
+#define WAVEFORMLOADER_H
+
+#include <QObject>
+#include <QList>
+#include <QMap>
+#include <QSet>
+#include <QString>
+#include <QUrl>
+
+#include "waveformpipeline.h"
+
+class QThread;
+class QByteArray;
+class QNetworkDiskCache;
+
+// Async orchestrator for track waveform data. Owns a worker QThread (idle I/O
+// priority) that runs WaveformPipeline decodes, caps in-flight requests at
+// idealThreadCount/2, dedupes in-flight requests by URL, and persists generated
+// blobs to a QNetworkDiskCache at CacheLocation/waveform keyed by the
+// percent-encoded source path. Replaying a track returns the cached blob
+// synchronously instead of re-decoding.
+class WaveformLoader : public QObject {
+  Q_OBJECT
+
+ public:
+  explicit WaveformLoader(QObject *parent = nullptr);
+  ~WaveformLoader() override;
+
+  enum class LoadStatus {
+    // The URL isn't a local file - waveform data can never be loaded.
+    CannotLoad,
+
+    // Waveform data was loaded and returned.
+    Loaded,
+
+    // Waveform data will be loaded in the background, a WaveformPipeline
+    // was returned that you can connect to the Finished() signal on.
+    WillLoadAsync
+  };
+
+  class LoadResult {
+   public:
+    LoadResult(const LoadStatus _status) : status(_status) {}
+    LoadResult(const LoadStatus _status, WaveformPipelinePtr _pipeline) : status(_status), pipeline(_pipeline) {}
+    LoadResult(const LoadStatus _status, const QByteArray &_data) : status(_status), data(_data) {}
+    LoadStatus status;
+    WaveformPipelinePtr pipeline;
+    QByteArray data;
+  };
+
+  LoadResult Load(const QUrl &url, const bool has_cue);
+
+ private:
+  static QUrl CacheUrlEntry(const QString &filename);
+  void RequestFinished(WaveformPipelinePtr pipeline, const QUrl &url);
+  void MaybeTakeNextRequest();
+
+ private:
+  QNetworkDiskCache *cache_;
+  QThread *thread_;
+
+  const int kMaxActiveRequests;
+
+  QMap<QUrl, WaveformPipelinePtr> requests_;
+  QList<QUrl> queued_requests_;
+  QSet<QUrl> active_requests_;
+};
+
+#endif  // WAVEFORMLOADER_H
