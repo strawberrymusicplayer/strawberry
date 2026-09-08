266e3a1cd3f00f080dc795a7a5845f231b2d3aa6
Lars Wendler <polynomial-c@gmx.de>
2026-09-08 11:21:22 +0200

    Fix build with tagfetcher being disabled
    Fixes #2310

--- src/settings/settingsdialog.cpp
+++ src/settings/settingsdialog.cpp
@@ -137,7 +137,9 @@ SettingsDialog::SettingsDialog(const SharedPtr<Player> player,
   AddPage(Page::Scrobbler, new ScrobblerSettingsPage(this, scrobbler, this), general);
   AddPage(Page::Covers, new CoversSettingsPage(this, cover_providers, this), general);
   AddPage(Page::Lyrics, new LyricsSettingsPage(this, lyrics_providers, this), general);
+#ifdef HAVE_TAGFETCHER
   AddPage(Page::Acoustid, new AcoustidSettingsPage(this, this), general);
+#endif
   AddPage(Page::Transcoding, new TranscoderSettingsPage(this, this), general);
   AddPage(Page::Proxy, new NetworkProxySettingsPage(this, this), general);
 
