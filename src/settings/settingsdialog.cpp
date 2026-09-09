ec7dffefb1f0015d071e63c9d02a6281bef44167
Jonas Kvinge <jonas@jkvinge.net>
2026-09-09 21:10:13 +0200

    Fix build with tagfetcher disabled
    Fixes #2310

--- src/settings/settingsdialog.cpp
+++ src/settings/settingsdialog.cpp
@@ -66,13 +66,15 @@
 #include "scrobblersettingspage.h"
 #include "coverssettingspage.h"
 #include "lyricssettingspage.h"
-#include "acoustidsettingspage.h"
 #include "transcodersettingspage.h"
 #include "networkproxysettingspage.h"
 #include "appearancesettingspage.h"
 #include "contextsettingspage.h"
 #include "notificationssettingspage.h"
 #include "globalshortcutssettingspage.h"
+#ifdef HAVE_TAGFETCHER
+#  include "acoustidsettingspage.h"
+#endif
 #ifdef HAVE_MOODBAR
 #  include "moodbarsettingspage.h"
 #endif
@@ -137,7 +139,9 @@ SettingsDialog::SettingsDialog(const SharedPtr<Player> player,
   AddPage(Page::Scrobbler, new ScrobblerSettingsPage(this, scrobbler, this), general);
   AddPage(Page::Covers, new CoversSettingsPage(this, cover_providers, this), general);
   AddPage(Page::Lyrics, new LyricsSettingsPage(this, lyrics_providers, this), general);
+#ifdef HAVE_TAGFETCHER
   AddPage(Page::Acoustid, new AcoustidSettingsPage(this, this), general);
+#endif
   AddPage(Page::Transcoding, new TranscoderSettingsPage(this, this), general);
   AddPage(Page::Proxy, new NetworkProxySettingsPage(this, this), general);
 
