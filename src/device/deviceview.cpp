54c9beadc7a2845d7ed9ecd991e6ed2656735ac2
Jonas Kvinge <jonas@jkvinge.net>
2026-09-25 01:35:18 +0200

    Improve detecting dark palette

--- src/device/deviceview.cpp
+++ src/device/deviceview.cpp
@@ -2,7 +2,7 @@
  * Strawberry Music Player
  * This file was part of Clementine.
  * Copyright 2010, David Sansome <me@davidsansome.com>
- * Copyright 2018-2021, Jonas Kvinge <jonas@jkvinge.net>
+ * Copyright 2018-2026, Jonas Kvinge <jonas@jkvinge.net>
  *
  * Strawberry is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -158,7 +158,7 @@ void DeviceItemDelegate::paint(QPainter *painter, const QStyleOptionViewItem &op
     painter->setPen(option.palette.color(QPalette::HighlightedText));
   }
   else {
-    if (Utilities::IsColorDark(option.palette.color(QPalette::Window))) {
+    if (Utilities::IsPaletteDark(option.palette, QPalette::Base, QPalette::Text)) {
       painter->setPen(option.palette.color(QPalette::Midlight).lighter().lighter());
     }
     else {
